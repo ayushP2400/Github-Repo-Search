d13b0de2a37d26434bdebc2ea7b577fe6c03fb38
ayushP2400 <ayush.pathak021@outlook.com>
2023-01-18 11:56:03 +0530

    removed github token from code and updated todo

--- Github-Repo-Search/Utility/Constants.swift
+++ Github-Repo-Search/Utility/Constants.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 class Constants {
-    static let githubAccessToken = "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"
+    static let githubAccessToken = "" // YOUR GITHUB TOKEN
     static let githubBaseURL = "https://api.github.com"
     
     enum GithubSearchEndpoints: String {
